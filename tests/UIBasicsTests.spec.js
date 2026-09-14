14beea95083f8adda5590297b37638f563cb8cf7
fullstackmachina <misscyborgtech@gmail.com>
2026-09-14 18:29:39 -0400

    finish the section 3

--- tests/UIBasicsTests.spec.js
+++ tests/UIBasicsTests.spec.js
@@ -1,3 +1,14 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
+test("Browser context", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/");
+  console.log(await page.title());
+});
 
-test("First Playwright Test", async () => {});
+test("Page", async ({ page }) => {
+  await page.goto("https://google.com/");
+  // get title - assertion
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
+});
